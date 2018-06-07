20e58dd2a3bc6907d20c2c4eb278ef71f981d289
mhollink <mhollink@quintor.nl>
2018-06-07 14:37:07 +0200

    renamed pagination component spec

--- assignment/staq-movies-frontend/src/app/shared/components/pagination/pagination.component.spec.ts
+++ assignment/staq-movies-frontend/src/app/shared/components/pagination/pagination.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaginationComponent } from './pagination.component';
 
-describe('KpnPaginationComponent', () => {
+describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
