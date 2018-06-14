a901cfee608229178f40c816ea77ceab8a584139
Marcel Hollink <marcel_Hollink@hotmail.com>
2018-06-14 20:25:17 +0200

    implemented missing service call on ratings service

--- assignment/staq-movies-frontend/src/app/shared/services/rating.service.ts
+++ assignment/staq-movies-frontend/src/app/shared/services/rating.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Rating} from '../models/types';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class RatingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  public getRatingsForMovie(movieId: number): Observable<Rating[]> {
+    return <Observable<Rating[]>> this.http.get(`api/movies/${movieId}/ratings`);
+  }
 }
